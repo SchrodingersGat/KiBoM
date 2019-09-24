a97f25de316fe8cd42ae9d69a4050c4024af7e32
Oliver Walters <oliver.henry.walters@gmail.com>
2019-09-24 20:33:32 +1000

    PEP fixes for xml_writer.py

--- bomlib/xml_writer.py
+++ bomlib/xml_writer.py
@@ -1,9 +1,3 @@
-import bomlib.columns as columns
-from bomlib.component import *
-from xml.etree import ElementTree
-from xml.dom import minidom
-from bomlib.preferences import BomPref
-
 """
 Write BoM out to an XML file
 filename = path to output file (must be a .xml)
@@ -12,6 +6,13 @@ net = netlist object
 headings = [list of headings to display in the BoM file]
 prefs = BomPref object
 """
+
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from xml.etree import ElementTree
+from xml.dom import minidom
+
 
 def WriteXML(filename, groups, net, headings, prefs):
 
@@ -38,7 +39,7 @@ def WriteXML(filename, groups, net, headings, prefs):
     attrib['Number_of_PCBs'] = str(prefs.boards)
     attrib['Total_Components'] = str(nBuild)
 
-    xml = ElementTree.Element('KiCad_BOM', attrib = attrib, encoding='utf-8')
+    xml = ElementTree.Element('KiCad_BOM', attrib=attrib, encoding='utf-8')
 
     for group in groups:
         if prefs.ignoreDNF and not group.isFitted():
@@ -48,16 +49,16 @@ def WriteXML(filename, groups, net, headings, prefs):
 
         attrib = {}
 
-        for i,h in enumerate(headings):
-            h = h.replace(' ','_') #replace spaces, xml no likey
-            h = h.replace('"','')
-            h = h.replace("'",'')
+        for i, h in enumerate(headings):
+            h = h.replace(' ', '_')  # Replace spaces, xml no likey
+            h = h.replace('"', '')
+            h = h.replace("'", '')
 
-            attrib[h] = str(row[i]).decode('ascii',errors='ignore')
+            attrib[h] = str(row[i]).decode('ascii', errors='ignore')
 
-        sub = ElementTree.SubElement(xml, "group", attrib=attrib)
+        # sub = ElementTree.SubElement(xml, "group", attrib=attrib)
 
-    with open(filename,"w") as output:
+    with open(filename, "w") as output:
         out = ElementTree.tostring(xml, encoding="utf-8")
         output.write(minidom.parseString(out).toprettyxml(indent="\t"))
 
