a8f0ddf9de5d50a407a20726358b10cfcb8c7f8f
Oliver Walters <oliver.henry.walters@gmail.com>
2019-09-24 21:19:02 +1000

    PEP fixes for component.py

--- bomlib/component.py
+++ bomlib/component.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from bomlib.columns import ColumnList
 from bomlib.preferences import BomPref
 import bomlib.units as units
@@ -5,16 +7,23 @@ from bomlib.sort import natural_sort
 import re
 import sys
 
-DNF = ["dnf",
-       "do not fit",
-       "nofit",
-       "not fitted",
-       "dnp",
-       "do not place",
-       "no stuff",
-       "nostuff",
-       "noload",
-       "do not load"]
+DNF = [
+    "dnf",
+    "dnl",
+    "dnp",
+    "do not fit",
+    "do not place",
+    "do not load",
+    "nofit",
+    "nostuff",
+    "noplace",
+    "noload",
+    "not fitted",
+    "not loaded",
+    "not placed",
+    "no stuff",
+]
+
 
 class Component():
     """Class for a component, aka 'comp' in the xml netlist file.
@@ -34,31 +43,34 @@ class Component():
         # Set to true when this component is included in a component group
         self.grouped = False
 
-    #compare the value of this part, to the value of another part (see if they match)
+    # Compare the value of this part, to the value of another part (see if they match)
     def compareValue(self, other):
-        #simple string comparison
-        if self.getValue().lower() == other.getValue().lower(): return True
+        # Simple string comparison
+        if self.getValue().lower() == other.getValue().lower():
+            return True
 
-        #otherwise, perform a more complicated value comparison
-        if units.compareValues(self.getValue(), other.getValue()): return True
+        # Otherwise, perform a more complicated value comparison
+        if units.compareValues(self.getValue(), other.getValue()):
+            return True
 
-        #Ignore value if both components are connectors
+        # Ignore value if both components are connectors
         if self.prefs.groupConnectors:
             if 'connector' in self.getLibName().lower() and 'connector' in other.getLibName().lower():
                 return True
 
-        #no match, return False
+        # No match, return False
         return False
 
-    #determine if two parts have the same name
+    # Determine if two parts have the same name
     def comparePartName(self, other):
         pn1 = self.getPartName().lower()
         pn2 = other.getPartName().lower()
 
-        #simple direct match
-        if pn1 == pn2: return True
+        # Simple direct match
+        if pn1 == pn2:
+            return True
 
-        #compare part aliases e.g. "c" to "c_small"
+        # Compare part aliases e.g. "c" to "c_small"
         for alias in self.prefs.aliases:
             if pn1 in alias and pn2 in alias:
                 return True
@@ -70,37 +82,39 @@ class Component():
         this_field = self.getField(field).lower()
         other_field = other.getField(field).lower()
 
-        #if blank comparisons are allowed
+        # If blank comparisons are allowed
         if this_field == "" or other_field == "":
             if not self.prefs.mergeBlankFields:
                 return False
 
-        if this_field == other_field: return True
+        if this_field == other_field:
+            return True
 
         return False
 
-    #Equivalency operator is used to determine if two parts are 'equal'
     def __eq__(self, other):
-        """Equlivalency operator, remember this can be easily overloaded"""
-
-        #'fitted' value must be the same for both parts
+        """
+        Equivalency operator is used to determine if two parts are 'equal'
+        """
+        
+        # 'fitted' value must be the same for both parts
         if self.isFitted() != other.isFitted():
             return False
 
-        if len(self.prefs.groups)==0:
+        if len(self.prefs.groups) == 0:
             return False
 
         for c in self.prefs.groups:
-            #perform special matches
+            # Perform special matches
             if c.lower() == ColumnList.COL_VALUE.lower():
                 if not self.compareValue(other):
                     return False
-            #match part name
+            # Match part name
             elif c.lower() == ColumnList.COL_PART.lower():
                 if not self.comparePartName(other):
                     return False
 
-            #generic match
+            # Generic match
             elif not self.compareField(other, c):
                     return False
 
@@ -109,25 +123,37 @@ class Component():
     def setLibPart(self, part):
         self.libpart = part
 
-    def getPrefix(self): #return the reference prefix
-        #e.g. if this component has a reference U12, will return "U"
+    def getPrefix(self):
+        """
+        Get the reference prefix
+        e.g. if this component has a reference U12, will return "U"
+        """
+        
         prefix = ""
 
         for c in self.getRef():
-            if c.isalpha(): prefix += c
-            else: break
+            if c.isalpha():
+                prefix += c
+            else:
+                break
 
         return prefix
 
-    def getSufix(self): #return the reference sufix #
-        #e.g. if this component has a reference U12, will return "12"
-        sufix = ""
+    def getSuffix(self):
+        """
+        Return the reference suffix #
+        e.g. if this component has a reference U12, will return "12"
+        """
+        
+        suffix = ""
 
         for c in self.getRef():
-            if c.isalpha(): sufix = ""
-            else: sufix += c
+            if c.isalpha():
+                suffix = ""
+            else:
+                suffix += c
 
-        return int(sufix)
+        return int(suffix)
 
     def getLibPart(self):
         return self.libpart
@@ -171,8 +197,6 @@ class Component():
                         in component itself
         """
 
-
-        #special fields
         fp = self.getFootprint().split(":")
 
         if name.lower() == ColumnList.COL_REFERENCE.lower():
@@ -189,7 +213,8 @@ class Component():
             if len(fp) > 1:
                 return fp[0].strip()
             else:
-                return "" # explicit empty return
+                # Explicit empty return
+                return ""
 
         elif name.lower() == ColumnList.COL_FP.lower():
             if len(fp) > 1:
@@ -208,7 +233,7 @@ class Component():
         elif name.lower() == ColumnList.COL_PART_LIB.lower():
             return self.getLibName().strip()
 
-        #other fields (case insensitive)
+        # Other fields (case insensitive)
         for f in self.getFieldNames():
             if f.lower() == name.lower():
                 field = self.element.get("field", "name", f)
@@ -218,7 +243,7 @@ class Component():
 
                 return field.strip()
 
-        #could not find a matching field
+        # Could not find a matching field
         return ""
 
     def getFieldNames(self):
@@ -227,28 +252,32 @@ class Component():
         The netlist format only includes fields with non-empty values.  So if a field
         is empty, it will not be present in the returned list.
         """
+
         fieldNames = []
+        
         fields = self.element.getChild('fields')
+        
         if fields:
             for f in fields.getChildren():
-                fieldNames.append( f.get('field','name') )
+                fieldNames.append(f.get('field', 'name'))
+        
         return fieldNames
 
     def getRef(self):
         return self.element.get("comp", "ref")
 
-    #determine if a component is FITTED or not
+    # Determine if a component is FITTED or not
     def isFitted(self):
 
         check = self.getField(self.prefs.configField).lower()
 
-
-        #check the value field first
+        # Check the value field first
         if self.getValue().lower() in DNF:
             return False
 
+        # Empty value means part is fitted
         if check == "":
-            return True #empty is fitted
+            return True
 
         opts = check.lower().split(",")
 
@@ -268,9 +297,6 @@ class Component():
                 break
             if opt.startswith("+"):
                 include = include or opt[1:] in [str(cfg) for cfg in self.prefs.pcbConfig]
-
-        # By default, part is fitted
-        fitted = include and not exclude
 
         return include and not exclude
 
@@ -293,19 +319,20 @@ class Component():
                 if re.search(regex, field_value, flags=re.IGNORECASE) is not None:
                     if self.prefs.verbose:
                         print("Excluding '{ref}': Field '{field}' ({value}) matched '{reg}'".format(
-                            ref = self.getRef(),
-                            field = field_name,
-                            value = field_value,
-                            reg = regex))
+                            ref=self.getRef(),
+                            field=field_name,
+                            value=field_value,
+                            reg=regex))
 
-                    return True #found a match
+                    # Found a match
+                    return True
 
-        #default, could not find any matches
+        # Default, could not find any matches
         return False
 
     def testRegInclude(self):
 
-        if len(self.prefs.regIncludes) == 0: #nothing to match against
+        if len(self.prefs.regIncludes) == 0:  # Nothing to match against
             return True
 
         for reg in self.prefs.regIncludes:
@@ -314,73 +341,78 @@ class Component():
                 field_name, regex = reg
                 field_value = self.getField(field_name)
 
-                print(field_name,field_value,regex)
+                print(field_name, field_value, regex)
 
                 if re.search(regex, field_value, flags=re.IGNORECASE) is not None:
                     if self.prefs.verbose:
                         print("")
 
-                    #found a match
+                    # Found a match
                     return True
 
-        #default, could not find a match
+        # Default, could not find a match
         return False
 
     def getFootprint(self, libraryToo=True):
         ret = self.element.get("footprint")
-        if ret =="" and libraryToo:
+        if ret == "" and libraryToo:
             if self.libpart:
                 ret = self.libpart.getFootprint()
         return ret
 
     def getDatasheet(self, libraryToo=True):
         ret = self.element.get("datasheet")
-        if ret =="" and libraryToo:
+        if ret == "" and libraryToo:
             ret = self.libpart.getDatasheet()
         return ret
 
     def getTimestamp(self):
         return self.element.get("tstamp")
 
+
 class joiner:
     def __init__(self):
-      self.stack = []
+        self.stack = []
 
     def add(self, P, N):
 
         if self.stack == []:
-            self.stack.append(((P,N),(P,N)))
+            self.stack.append(((P, N), (P, N)))
             return
 
         S, E = self.stack[-1]
 
-        if N == E[1]+1:
-            self.stack[-1] = (S, (P,N))
+        if N == E[1] + 1:
+            self.stack[-1] = (S, (P, N))
         else:
-            self.stack.append(((P,N),(P,N)))
+            self.stack.append(((P, N), (P, N)))
 
     def flush(self, sep, N=None, dash='-'):
+        
         refstr = u''
         c = 0
+
         for Q in self.stack:
-            if bool(N) and c!=0 and c%N==0:
-                refstr+=u'\n'
-            elif c!=0:
-                refstr+=sep
+            if bool(N) and c != 0 and c % N == 0:
+                refstr += u'\n'
+            elif c != 0:
+                refstr += sep
 
             S, E = Q
+
             if S == E:
-                refstr+="%s%d"%S
-                c+=1
+                refstr += "%s%d" % S
+                c += 1
             else:
-                #do we have space?
-                if bool(N) and (c+1)%N==0: #no
-                    refstr+=u'\n'
+                # Do we have space?
+                if bool(N) and (c + 1) % N == 0:
+                    refstr += u'\n'
                     c += 1
 
-                refstr+="%s%d%s%s%d"%(S[0],S[1],dash,E[0],E[1])
-                c+=2
+                refstr += "%s%d%s%s%d" % (S[0], S[1], dash, E[0], E[1])
+                c += 2
         return refstr
+
 
 class ComponentGroup():
 
@@ -389,7 +421,7 @@ class ComponentGroup():
     """
     def __init__(self, prefs=None):
         self.components = []
-        self.fields = dict.fromkeys(ColumnList._COLUMNS_DEFAULT)    #columns loaded from KiCad
+        self.fields = dict.fromkeys(ColumnList._COLUMNS_DEFAULT)  # Columns loaded from KiCad
 
         if not prefs:
             prefs = BomPref()
@@ -398,31 +430,39 @@ class ComponentGroup():
 
     def getField(self, field):
 
-        if not field in self.fields.keys(): return ""
-        if not self.fields[field]: return ""
+        if field not in self.fields.keys():
+            return ""
+        
+        if not self.fields[field]:
+            return ""
+        
         return u''.join((self.fields[field]))
 
     def getCount(self):
         return len(self.components)
 
-    #Test if a given component fits in this group
+    # Test if a given component fits in this group
     def matchComponent(self, c):
-        if len(self.components) == 0: return True
-        if c == self.components[0]: return True
+        if len(self.components) == 0:
+            return True
+        if c == self.components[0]:
+            return True
 
         return False
 
-    #test if a given component is already contained in this grop
     def containsComponent(self, c):
-        if self.matchComponent(c) == False: return False
+        # Test if a given component is already contained in this grop
+        if not self.matchComponent(c):
+            return False
 
         for comp in self.components:
-            if comp.getRef() == c.getRef(): return True
+            if comp.getRef() == c.getRef():
+                return True
 
         return False
 
-    #add a component to the group
     def addComponent(self, c):
+        # Add a component to the group
 
         if len(self.components) == 0:
             self.components.append(c)
@@ -434,58 +474,58 @@ class ComponentGroup():
     def isFitted(self):
         return any([c.isFitted() for c in self.components])
 
-    #return a list of the components
     def getRefs(self):
-        #print([c.getRef() for c in self.components]))
-        #return " ".join([c.getRef() for c in self.components])
+        # Return a list of the components
         return " ".join([c.getRef() for c in self.components])
 
     def getAltRefs(self, wrapN=None):
         S = joiner()
 
         for n in self.components:
-                P, N = (n.getPrefix(), n.getSufix())
-                S.add(P,N)
+            P, N = (n.getPrefix(), n.getSuffix())
+            S.add(P, N)
 
         return S.flush(' ', N=wrapN)
 
-    #sort the components in correct order
+    # Sort the components in correct order
     def sortComponents(self):
         self.components = sorted(self.components, key=lambda c: natural_sort(c.getRef()))
 
-    #update a given field, based on some rules and such
+    # Update a given field, based on some rules and such
     def updateField(self, field, fieldData):
 
-        #protected fields cannot be overwritten
-        if field in ColumnList._COLUMNS_PROTECTED: return
-
-        if (field == None or field == ""): return
-        elif fieldData == "" or fieldData == None:
+        # Protected fields cannot be overwritten
+        if field in ColumnList._COLUMNS_PROTECTED:
             return
 
-        if (not field in self.fields.keys()) or (self.fields[field] == None) or (self.fields[field] == ""):
+        if field is None or field == "":
+            return
+        elif fieldData == "" or fieldData is None:
+            return
+
+        if (field not in self.fields.keys()) or (self.fields[field] is None) or (self.fields[field] == ""):
             self.fields[field] = fieldData
         elif fieldData.lower() in self.fields[field].lower():
             return
         else:
             print("Field conflict: ({refs}) [{name}] : '{flds}' <- '{fld}'".format(
-                refs = self.getRefs(),
-                name = field,
-                flds = self.fields[field],
-                fld = fieldData))
+                refs=self.getRefs(),
+                name=field,
+                flds=self.fields[field],
+                fld=fieldData))
             self.fields[field] += " " + fieldData
 
     def updateFields(self, usealt=False, wrapN=None):
         for c in self.components:
             for f in c.getFieldNames():
 
-                #these columns are handled explicitly below
+                # These columns are handled explicitly below
                 if f in ColumnList._COLUMNS_PROTECTED:
                     continue
 
                 self.updateField(f, c.getField(f))
 
-        #update 'global' fields
+        # Update 'global' fields
         if usealt:
             self.fields[ColumnList.COL_REFERENCE] = self.getAltRefs(wrapN)
         else:
@@ -494,7 +534,7 @@ class ComponentGroup():
         q = self.getCount()
         self.fields[ColumnList.COL_GRP_QUANTITY] = "{n}{dnf}".format(
             n=q,
-            dnf = " (DNF)" if not self.isFitted() else "")
+            dnf=" (DNF)" if not self.isFitted() else "")
 
         self.fields[ColumnList.COL_GRP_BUILD_QUANTITY] = str(q * self.prefs.boards) if self.isFitted() else "0"
         self.fields[ColumnList.COL_VALUE] = self.components[0].getValue()
@@ -516,8 +556,8 @@ class ComponentGroup():
             self.fields[ColumnList.COL_FP_LIB] = ""
             self.fields[ColumnList.COL_FP] = ""
 
-    #return a dict of the KiCad data based on the supplied columns
-    #NOW WITH UNICODE SUPPORT!
+    # Return a dict of the KiCad data based on the supplied columns
+    # NOW WITH UNICODE SUPPORT!
     def getRow(self, columns):
         row = []
         for key in columns:
@@ -527,7 +567,7 @@ class ComponentGroup():
                 val = ""
             else:
                 val = u'' + val
-                if (sys.version_info[0] < 3):
+                if sys.version_info[0] < 3:
                     val = val.encode('utf-8')
 
             row.append(val)
