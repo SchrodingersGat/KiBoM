3ee1a79806d55121542c610f22ec075752971952
Oliver Walters <oliver.henry.walters@gmail.com>
2019-09-24 21:04:20 +1000

    PEP fixes for csv_writer.py

--- bomlib/csv_writer.py
+++ bomlib/csv_writer.py
@@ -1,28 +1,25 @@
 # _*_ coding:latin-1 _*_
 
 import csv
-import bomlib.columns as columns
-from bomlib.component import *
-import os, shutil
-from bomlib.preferences import BomPref
+import os
 import sys
 
-"""
-Write BoM out to a CSV file
-filename = path to output file (must be a .csv, .txt or .tsv file)
-groups = [list of ComponentGroup groups]
-net = netlist object
-headings = [list of headings to display in the BoM file]
-prefs = BomPref object
-"""
 
 def WriteCSV(filename, groups, net, headings, prefs):
+    """
+    Write BoM out to a CSV file
+    filename = path to output file (must be a .csv, .txt or .tsv file)
+    groups = [list of ComponentGroup groups]
+    net = netlist object
+    headings = [list of headings to display in the BoM file]
+    prefs = BomPref object
+    """
 
     filename = os.path.abspath(filename)
 
-    #delimeter is assumed from file extension
-	#override delimiter if separator specified
-    if prefs.separatorCSV != None:
+    # Delimeter is assumed from file extension
+    # Override delimiter if separator specified
+    if prefs.separatorCSV is not None:
         delimiter = prefs.separatorCSV
     else:
         if filename.endswith(".csv"):
@@ -53,16 +50,17 @@ def WriteCSV(filename, groups, net, headings, prefs):
     count = 0
     rowCount = 1
 
-    for i, group in enumerate(groups):
-        if prefs.ignoreDNF and not group.isFitted(): continue
+    for group in groups:
+        if prefs.ignoreDNF and not group.isFitted():
+            continue
 
         row = group.getRow(headings)
 
         if prefs.numberRows:
             row = [str(rowCount)] + row
 
-        #deal with unicode characters
-        #row = [el.decode('latin-1') for el in row]
+        # Deal with unicode characters
+        # Row = [el.decode('latin-1') for el in row]
         writer.writerow(row)
 
         try:
@@ -73,21 +71,21 @@ def WriteCSV(filename, groups, net, headings, prefs):
         rowCount += 1
 
     if not prefs.hidePcbInfo:
-        #blank rows
+        # Add some blank rows
         for i in range(5):
             writer.writerow([])
 
-        writer.writerow(["Component Groups:",nGroups])
-        writer.writerow(["Component Count:",nTotal])
+        writer.writerow(["Component Groups:", nGroups])
+        writer.writerow(["Component Count:", nTotal])
         writer.writerow(["Fitted Components:", nFitted])
-        writer.writerow(["Number of PCBs:",prefs.boards])
+        writer.writerow(["Number of PCBs:", prefs.boards])
         writer.writerow(["Total components:", nBuild])
-        writer.writerow(["Schematic Version:",net.getVersion()])
-        writer.writerow(["Schematic Date:",net.getSheetDate()])
-        writer.writerow(["PCB Variant:",' + '.join(prefs.pcbConfig)])
-        writer.writerow(["BoM Date:",net.getDate()])
-        writer.writerow(["Schematic Source:",net.getSource()])
-        writer.writerow(["KiCad Version:",net.getTool()])
+        writer.writerow(["Schematic Version:", net.getVersion()])
+        writer.writerow(["Schematic Date:", net.getSheetDate()])
+        writer.writerow(["PCB Variant:", ' + '.join(prefs.pcbConfig)])
+        writer.writerow(["BoM Date:", net.getDate()])
+        writer.writerow(["Schematic Source:", net.getSource()])
+        writer.writerow(["KiCad Version:", net.getTool()])
 
     f.close()
 
