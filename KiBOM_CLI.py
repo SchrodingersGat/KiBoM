cd59842e12f75b1221a1e11b4549041218f1e9bc
Oliver Walters <oliver.henry.walters@gmail.com>
2019-09-24 21:34:08 +1000

    Final PEP fixes

--- KiBOM_CLI.py
+++ KiBOM_CLI.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 """
     @package
     KiBOM - Bill of Materials generation for KiCad
@@ -16,48 +18,46 @@
 
 from __future__ import print_function
 
-import re
-import csv
 import sys
 import os
-import shutil
 
 import argparse
 
-# Optional modules
+from bomlib.columns import ColumnList
+from bomlib.netlist_reader import netlist
+from bomlib.bom_writer import WriteBoM
+from bomlib.preferences import BomPref
 
 try:
-    import xlsxwriter
+    import xlsxwriter  # noqa: F401
 except:
     xlsxwriter_available = False
 else:
     xlsxwriter_available = True
 
-#
 here = os.path.abspath(os.path.dirname(sys.argv[0]))
 
 sys.path.append(here)
-sys.path.append(os.path.join(here,"KiBOM"))
+sys.path.append(os.path.join(here, "KiBOM"))
 
-from bomlib.columns import ColumnList
-from bomlib.netlist_reader import *
-from bomlib.bom_writer import *
-from bomlib.preferences import BomPref
 
 verbose = False
+
 
 def close(*arg):
     print(*arg)
     sys.exit(0)
 
-# Simple debug message handler
+
 def say(*arg):
+    # Simple debug message handler
     if verbose:
         print(*arg)
 
+
 def isExtensionSupported(filename):
     result = False
-    extensions = [".xml",".csv",".txt",".tsv",".html"]
+    extensions = [".xml", ".csv", ".txt", ".tsv", ".html"]
     if xlsxwriter_available:
         extensions.append(".xlsx")
     for e in extensions:
@@ -66,50 +66,51 @@ def isExtensionSupported(filename):
             break
     return result
 
+
 def writeVariant(variant, subdirectory):
     if variant is not None:
-        pref.pcbConfig = variant.strip().split(',') 
+        pref.pcbConfig = variant.strip().split(',')
         
     print("PCB variant: ", ", ".join(pref.pcbConfig))
 
-    #write preference file back out (first run will generate a file with default preferences)
+    # Write preference file back out (first run will generate a file with default preferences)
     if not have_cfile:
         pref.Write(config_file)
-        say("Writing preferences file %s"%(config_file,))
+        say("Writing preferences file %s" % (config_file,))
 
-    #individual components
+    # Individual components
     components = []
 
-    #component groups
+    # Component groups
     groups = []
 
-    #read out the netlist
-    net = netlist(input_file, prefs = pref)
+    # Read out the netlist
+    net = netlist(input_file, prefs=pref)
 
-    #extract the components
+    # Extract the components
     components = net.getInterestingComponents()
 
-    #group the components
+    # Group the components
     groups = net.groupComponents(components)
 
     columns = ColumnList(pref.corder)
 
-    #read out all available fields
+    # Read out all available fields
     for g in groups:
         for f in g.fields:
             columns.AddColumn(f)
 
-    #don't add 'boards' column if only one board is specified
+    # Don't add 'boards' column if only one board is specified
     if pref.boards <= 1:
         columns.RemoveColumn(ColumnList.COL_GRP_BUILD_QUANTITY)
-        say("Removing:",ColumnList.COL_GRP_BUILD_QUANTITY)
+        say("Removing:", ColumnList.COL_GRP_BUILD_QUANTITY)
 
-    #Finally, write the BoM out to file
+    # Finally, write the BoM out to file
     if write_to_bom:
         output_file = args.output
 
         if output_file is None:
-            output_file = input_file.replace(".xml",".csv")
+            output_file = input_file.replace(".xml", ".csv")
 
         output_path, output_name = os.path.split(output_file)
         output_name, output_ext = os.path.splitext(output_name)
@@ -130,27 +131,28 @@ def writeVariant(variant, subdirectory):
             file_name = file_name.replace("%V", "")
 
         if args.subdirectory is not None:
-            output_path = os.path.join(output_path,args.subdirectory)
+            output_path = os.path.join(output_path, args.subdirectory)
             if not os.path.exists(os.path.abspath(output_path)):
                 os.makedirs(os.path.abspath(output_path))
 
-        output_file = os.path.join(output_path,file_name+output_ext)
+        output_file = os.path.join(output_path, file_name + output_ext)
         output_file = os.path.abspath(output_file)
 
-        say("Output:",output_file)
+        say("Output:", output_file)
 
         return WriteBoM(output_file, groups, net, columns.columns, pref)
+
 
 parser = argparse.ArgumentParser(description="KiBOM Bill of Materials generator script")
 
 parser.add_argument("netlist", help='xml netlist file. Use "%%I" when running from within KiCad')
-parser.add_argument("output",  default="", help='BoM output file name.\nUse "%%O" when running from within KiCad to use the default output name (csv file).\nFor e.g. HTML output, use "%%O.html"')
+parser.add_argument("output", default="", help='BoM output file name.\nUse "%%O" when running from within KiCad to use the default output name (csv file).\nFor e.g. HTML output, use "%%O.html"')
 parser.add_argument("-n", "--number", help="Number of boards to build (default = 1)", type=int, default=None)
 parser.add_argument("-v", "--verbose", help="Enable verbose output", action='count')
 parser.add_argument("-r", "--variant", help="Board variant(s), used to determine which components are output to the BoM. Comma-separate for multiple.", type=str, default=None)
 parser.add_argument("-d", "--subdirectory", help="Subdirectory within which to store the generated BoM files.", type=str, default=None)
 parser.add_argument("--cfg", help="BoM config file (script will try to use 'bom.ini' if not specified here)")
-parser.add_argument("-s","--separator",help="CSV Separator (default ',')",type=str, default=None)
+parser.add_argument("-s", "--separator", help="CSV Separator (default ',')", type=str, default=None)
 
 args = parser.parse_args()
 
@@ -163,35 +165,36 @@ verbose = args.verbose is not None
 
 input_file = os.path.abspath(input_file)
 
-say("Input:",input_file)
+say("Input:", input_file)
 
-#look for a config file!
-#bom.ini by default
+# Look for a config file!
+# bom.ini by default
 ini = os.path.abspath(os.path.join(os.path.dirname(input_file), "bom.ini"))
 
-config_file = ini #default value
-#user can overwrite with a specific config file
+# Default value
+config_file = ini
+
+# User can overwrite with a specific config file
 if args.cfg:
     config_file = args.cfg
 
-#read preferences from file. If file does not exists, default preferences will be used
+# Read preferences from file. If file does not exists, default preferences will be used
 pref = BomPref()
 
 have_cfile = os.path.exists(config_file)
 if have_cfile:
     pref.Read(config_file)
-    say("Config:",config_file)
+    say("Config:", config_file)
 
-#pass available modules
+# Pass available modules
 pref.xlsxwriter_available = xlsxwriter_available
 
-#pass various command-line options through
+# Pass various command-line options through
 pref.verbose = verbose
 if args.number is not None:
     pref.boards = args.number
 pref.separatorCSV = args.separator
 
-#todo
 write_to_bom = True
 
 if args.variant is not None:
