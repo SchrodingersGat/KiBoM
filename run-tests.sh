ab1c2912214ac14ae4fc9c786d4bdf3cdf34150c
Oliver Walters <oliver.henry.walters@gmail.com>
2020-03-13 00:37:20 +1100

    Run pip as sudo

--- run-tests.sh
+++ run-tests.sh
@@ -8,6 +8,9 @@ coverage run -a KiBOM_CLI.py test/kibom-test.xml test/bom-out.csv
 # Generate a html file
 coverage run -a KiBOM_CLI.py test/kibom-test.xml test/bom-out.html
 
+# Generate an XML file
+coverage run -a KiBOM_CLI.py test/kibom-test.xml test/bom-out.xml
+
 # Generate an XLSX file
 coverage run -a KiBOM_CLI.py test/kibom-test.xml test/bom-out.xlsx
 
