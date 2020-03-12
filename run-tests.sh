9e1a240d1682e9a917a8eb6da1af8bb301ec6ec9
Oliver Walters <oliver.henry.walters@gmail.com>
2020-03-13 00:30:33 +1100

    Install python3-coverage module

--- run-tests.sh
+++ run-tests.sh
@@ -3,7 +3,14 @@
 coverage erase
 
 # Run a simple test
-coverage run -a KiBOM_CLI.py test/kibom-test.xml test/bom-out
+coverage run -a KiBOM_CLI.py test/kibom-test.xml test/bom-out.csv
+
+# Generate a html file
+coverage run -a KiBOM_CLI.py test/kibom-test.xml test/bom-out.html
+
+# Generate an XLSX file
+coverage run -a KiBOM_CLI.py test/kibom-test.xml test/bom-out.xlsx
+
 # Generate HTML code coverage output
 coverage html
 
