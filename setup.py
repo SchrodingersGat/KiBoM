0b08a3cf790233dfa4fb0af213621f6548840755
Salvador E. Tropea <salvador@inti.gob.ar>
2020-03-13 08:45:12 -0300

    Made setup.py be "flake8" compatible

--- setup.py
+++ setup.py
@@ -15,32 +15,32 @@ here = os.path.abspath(os.path.dirname(__file__))
 # Import the README and use it as the long-description.
 # Note: this will only work if 'README.md' is present in your MANIFEST.in file!
 with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-     long_description = '\n' + f.read()
+    long_description = '\n' + f.read()
 about = {}
 with open(os.path.join(here, NAME_PKG, 'version.py')) as f:
-     exec(f.read(), about)
+    exec(f.read(), about)
 
 setup(name=NAME,
-    version=about['KIBOM_VERSION'],
-    description=DESCRIPTION,
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    author=AUTHOR,
-    author_email=EMAIL,
-    url=URL,
-    packages=[NAME_PKG],
-    package_dir={NAME_PKG: NAME_PKG},
-    scripts=['KiBOM_CLI.py'],
-    install_requires=['xlswriter'],
-    classifiers = ['Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 2',
-        'Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)',
-    ],
-    platforms   = 'POSIX',
-    license     = 'MIT'
-)
+      version=about['KIBOM_VERSION'],
+      description=DESCRIPTION,
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author=AUTHOR,
+      author_email=EMAIL,
+      url=URL,
+      packages=[NAME_PKG],
+      package_dir={NAME_PKG: NAME_PKG},
+      scripts=['KiBOM_CLI.py'],
+      install_requires=['xlswriter'],
+      classifiers=['Development Status :: 5 - Production/Stable',
+                   'Environment :: Console',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Natural Language :: English',
+                   'Operating System :: POSIX :: Linux',
+                   'Programming Language :: Python :: 2',
+                   'Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)',
+                   ],
+      platforms='POSIX',
+      license='MIT'
+      )
