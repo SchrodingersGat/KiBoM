63f99a5e5e932979c440b1338c6dcaab38bb33ec
Salvador E. Tropea <salvador@inti.gob.ar>
2020-07-14 21:01:40 -0300

    Migrated to pytest the CSV and HTML tests
    The CSV is a little bit stronger (IMHO the original isn't really
    testing because it fails to split the correct field).
    The HTML test is new, it does a very naive HTML parse but is
    better than nothing.

---
+++ tests/test_bom/test_simple.py
@@ -0,0 +1,52 @@
+"""
+Simple tests
+
+For debug information use:
+pytest-3 --log-cli-level debug
+
+"""
+
+import os
+import sys
+import logging
+# Look for the 'utils' module from where the script is running
+prev_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if prev_dir not in sys.path:
+    sys.path.insert(0, prev_dir)
+# Utils import
+from utils import context
+
+BOM_DIR = 'BoM'
+KIBOM_TEST_COMPONENTS = ['C1', 'C2', 'C3', 'C4', 'R1', 'R2', 'R3', 'R4', 'R5', 'R7', 'R8', 'R9', 'R10']
+
+def test_bom_simple_csv():
+    prj = 'kibom-test'
+    ext = 'csv'
+    ctx = context.TestContext('BoMSimpleCSV', prj, ext, BOM_DIR)
+    ctx.run(no_config_file=True)
+    out = prj+'_bom_A.'+ext
+    rows, components = ctx.load_csv(out)
+    assert len(rows) == 5
+    assert len(components) == 13
+    assert 'R6' not in components
+    for c in KIBOM_TEST_COMPONENTS:
+        assert c in components
+    ctx.clean_up()
+
+
+def test_bom_simple_html():
+    prj = 'kibom-test'
+    ext = 'html'
+    ctx = context.TestContext('BoMSimpleHTML', prj, ext, BOM_DIR)
+    ctx.run(no_config_file=True)
+    out = prj+'_bom_A.'+ext
+    rows, components, dnf = ctx.load_html(out)
+    assert len(rows) == 6
+    assert len(components) == 13
+    assert 'R6' not in components
+    for c in KIBOM_TEST_COMPONENTS:
+        assert c in components
+    assert len(dnf) == 1
+    assert 'R6' in dnf
+    ctx.clean_up()
+
---
+++ tests/utils/context.py
@@ -0,0 +1,261 @@
+import os
+import shutil
+import tempfile
+import logging
+import subprocess
+import re
+import pytest
+from glob import glob
+from pty import openpty
+
+COVERAGE_SCRIPT = 'python3-coverage'
+KICAD_NETLIST_EXT = '.xml'
+KICAD_SCH_EXT = '.sch'
+REF_DIR = 'tests/reference'
+
+
+MODE_SCH = 1
+MODE_PCB = 0
+
+
+class TestContext(object):
+
+    def __init__(self, test_name, prj_name, ext, sub_dir, config_name=None):
+        # We are using PCBs
+        self.mode = MODE_PCB
+        # The name used for the test output dirs and other logging
+        self.test_name = test_name
+        # The name of the PCB board file
+        self.prj_name = prj_name
+        # The actual board file that will be loaded
+        self._get_netlist_file()
+        # The YAML file we'll use
+        self._get_config_name(config_name)
+        # The actual output dir for this run
+        self._set_up_output_dir(pytest.config.getoption('test_dir'))
+        # Output format
+        self.ext = ext
+        # stdout and stderr from the run
+        self.out = None
+        self.err = None
+        self.proc = None
+
+    def get_board_dir(self):
+        this_dir = os.path.dirname(os.path.realpath(__file__))
+        return os.path.join(this_dir, '../input_samples')
+
+    def _get_netlist_file(self):
+        self.netlist_file = os.path.abspath(os.path.join(self.get_board_dir(), self.prj_name + KICAD_NETLIST_EXT))
+        self.sch_file = os.path.abspath(os.path.join(self.get_board_dir(), self.prj_name + KICAD_SCH_EXT))
+        logging.info('KiCad file: '+self.netlist_file)
+        assert os.path.isfile(self.netlist_file)
+
+    def _get_config_dir(self):
+        this_dir = os.path.dirname(os.path.realpath(__file__))
+        return os.path.join(this_dir, '../input_samples')
+
+    def _get_config_name(self, name):
+        skip_test = False
+        if not name:
+           name = 'bom'
+           skip_test = True
+        self.config_file = os.path.abspath(os.path.join(self._get_config_dir(), name+'.ini'))
+        logging.info('Config file: '+self.config_file)
+        assert skip_test or os.path.isfile(self.config_file)
+
+    def _set_up_output_dir(self, test_dir):
+        if test_dir:
+            self.output_dir = os.path.join(test_dir, self.test_name)
+            os.makedirs(self.output_dir, exist_ok=True)
+            self._del_dir_after = False
+        else:
+            # create a tmp dir
+            self.output_dir = tempfile.mkdtemp(prefix='tmp-kiplot-'+self.test_name+'-')
+            self._del_dir_after = True
+        logging.info('Output dir: '+self.output_dir)
+
+    def clean_up(self):
+        logging.debug('Clean-up')
+        if self._del_dir_after:
+            logging.debug('Removing dir')
+            shutil.rmtree(self.output_dir)
+        # We don't have a project, and we don't want one
+        pro = os.path.join(self.get_board_dir(), self.prj_name+'.pro')
+        if os.path.isfile(pro):
+            os.remove(pro)
+        # We don't have a footprint cache, and we don't want one
+        fp_cache = os.path.join(self.get_board_dir(), 'fp-info-cache')
+        if os.path.isfile(fp_cache):
+            os.remove(fp_cache)
+
+    def get_out_path(self, filename):
+        return os.path.join(self.output_dir, filename)
+
+    def expect_out_file(self, filename):
+        file = self.get_out_path(filename)
+        assert os.path.isfile(file), file
+        assert os.path.getsize(file) > 0
+        logging.debug(filename+' OK')
+        return file
+
+    def load_csv(self, filename):
+        file = self.expect_out_file(filename)
+        rows = []
+        with open(file) as f:
+            f.readline()  # Skip header
+            for line in f:
+                line = line.rstrip()
+                if not line:
+                    break
+                rows.append(line)
+        components = []
+        for r in rows:
+            fields = r.split(',')
+            comps = fields[3].split(' ')
+            components.extend(comps)
+        return rows, components
+
+    def load_html(self, filename):
+        file = self.expect_out_file(filename)
+        with open(file) as f:
+            html = f.read()
+        rows = []
+        components = []
+        components_dnf = []
+        prev = 0
+        dnf = False
+        for entry in re.finditer(r'<tr>\s+<td.*>(\d+)<\/td>\s+<td.*>([^<]+)</td>\s+'+
+            r'<td.*>([^<]+)</td>\s+<td.*>([^<]+)</td>\s+<td.*>([^<]+)</td>\s+', html):
+            cur = int(entry.group(1))
+            if cur < prev:
+                dnf = True
+            rows.append(entry.group(0))
+            if dnf:
+                components_dnf.extend(entry.group(4).split(' '))
+            else:
+                components.extend(entry.group(4).split(' '))
+            prev = cur
+        return rows, components, components_dnf
+
+    def dont_expect_out_file(self, filename):
+        file = self.get_out_path(filename)
+        assert not os.path.isfile(file)
+
+    def create_dummy_out_file(self, filename):
+        file = self.get_out_path(filename)
+        with open(file, 'w') as f:
+            f.write('Dummy file\n')
+
+    def run(self, ret_val=None, extra=None, use_a_tty=False, filename=None,
+            no_config_file=False, chdir_out=False, no_verbose=False):
+        logging.debug('Running '+self.test_name)
+        # Change the command to be local and add the board and output arguments
+        cmd = [COVERAGE_SCRIPT, 'run', '-a']
+        if chdir_out:
+            cmd.append('--rcfile=../../.coveragerc')
+            os.environ['COVERAGE_FILE'] = os.path.join(os.getcwd(), '.coverage')
+        cmd.append(os.path.abspath(os.path.dirname(os.path.abspath(__file__))+'/../../KiBOM_CLI.py'))
+        if not no_verbose:
+            cmd.append('-vv')
+        if not no_config_file:
+            cmd = cmd+['--cfg', self.config_file]
+        if extra is not None:
+            cmd = cmd+extra
+        # This changed in 1.7.x the path indicated in the output name is ignored.
+        # The only way is specify an absolute path here.
+        cmd = cmd+['-d', os.path.abspath(self.output_dir)]
+        cmd.append(filename if filename else self.netlist_file)
+        cmd.append(self.prj_name+'.'+self.ext)
+        logging.debug(cmd)
+        out_filename = self.get_out_path('output.txt')
+        err_filename = self.get_out_path('error.txt')
+        if use_a_tty:
+            # This is used to test the coloured logs, we need stderr to be a TTY
+            master, slave = openpty()
+            f_err = slave
+            f_out = slave
+        else:
+            # Redirect stdout and stderr to files
+            f_out = os.open(out_filename, os.O_RDWR | os.O_CREAT)
+            f_err = os.open(err_filename, os.O_RDWR | os.O_CREAT)
+        # Run the process
+        if chdir_out:
+            cwd = os.getcwd()
+            os.chdir(self.output_dir)
+        process = subprocess.Popen(cmd, stdout=f_out, stderr=f_err)
+        if chdir_out:
+            os.chdir(cwd)
+            del os.environ['COVERAGE_FILE']
+        ret_code = process.wait()
+        logging.debug('ret_code '+str(ret_code))
+        if use_a_tty:
+            self.err = os.read(master, 10000)
+            self.err = self.err.decode()
+            self.out = self.err
+        exp_ret = 0 if ret_val is None else ret_val
+        assert ret_code == exp_ret
+        if use_a_tty:
+            os.close(master)
+            os.close(slave)
+            with open(out_filename, 'w') as f:
+                f.write(self.out)
+            with open(err_filename, 'w') as f:
+                f.write(self.out)
+        else:
+            # Read stdout
+            os.lseek(f_out, 0, os.SEEK_SET)
+            self.out = os.read(f_out, 10000)
+            os.close(f_out)
+            self.out = self.out.decode()
+            # Read stderr
+            os.lseek(f_err, 0, os.SEEK_SET)
+            self.err = os.read(f_err, 10000)
+            os.close(f_err)
+            self.err = self.err.decode()
+
+    def search_out(self, text):
+        m = re.search(text, self.out, re.MULTILINE)
+        return m
+
+    def search_err(self, text):
+        m = re.search(text, self.err, re.MULTILINE)
+        return m
+
+    def search_in_file(self, file, texts):
+        logging.debug('Searching in "'+file+'" output')
+        with open(self.get_out_path(file)) as f:
+            txt = f.read()
+        res = []
+        for t in texts:
+            logging.debug('- r"'+t+'"')
+            m = re.search(t, txt, re.MULTILINE)
+            assert m
+            # logging.debug(' '+m.group(0))
+            res.append(m.groups())
+        return res
+
+    def search_not_in_file(self, file, texts):
+        logging.debug('Searching not in "'+file+'" output')
+        with open(self.get_out_path(file)) as f:
+            txt = f.read()
+        for t in texts:
+            logging.debug('- r"'+t+'"')
+            m = re.search(t, txt, re.MULTILINE)
+            assert m is None
+
+    def compare_txt(self, text, reference=None, diff='diff.txt'):
+        if reference is None:
+            reference = text
+        cmd = ['/bin/sh', '-c', 'diff -ub '+os.path.join(REF_DIR, reference)+' ' +
+               self.get_out_path(text)+' > '+self.get_out_path(diff)]
+        logging.debug('Comparing texts with: '+str(cmd))
+        res = subprocess.call(cmd)
+        assert res == 0
+
+    def filter_txt(self, file, pattern, repl):
+        fname = self.get_out_path(file)
+        with open(fname) as f:
+            txt = f.read()
+        with open(fname, 'w') as f:
+            f.write(re.sub(pattern, repl, txt))
+
